c22994082841a2c250b0df6ca01f7723c77edbd9
panwei <wei.pan@unimedsci.com>
2020-05-13 13:50:15 +0800

    Initial Commit

---
+++ AModule/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AModule
+//
+//  Created by panwei on 5/13/20.
+//  Copyright © 2020 panwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
